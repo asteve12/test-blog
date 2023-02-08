a627d50a78c8c9523e7213b76cda98ea9b7bbc33
xlassix <awhy14539@gmail.com>
2023-02-08 11:09:46 +0100

    markdown

--- src/component/blogDetails/BlogDetailContent.tsx
+++ src/component/blogDetails/BlogDetailContent.tsx
@@ -1,53 +1,59 @@
-import {Box, Flex, Heading, HStack, Image, Text} from "@chakra-ui/react"
-import { BlogTitle } from "../../shared/blogTitlteCard"
-import { TimerCard } from "../../shared/TimeCard"
-import { Socials } from "./socials"
-
-
-
-
+import { Box, Flex, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { BlogTitle } from "../../shared/blogTitlteCard";
+import { TimerCard } from "../../shared/TimeCard";
+import { Socials } from "./socials";
 
 type IBlogDetailContent = {
-    title: string,
-    content: string,
-  timeToRead: number  | void,
-   
-}
+  title: string;
+  content: string;
+  timeToRead: number | void;
+};
 
-
-
-export const BlogDetailContent = ({title,content,timeToRead}:IBlogDetailContent) => {
- 
- 
-    
-    return (<Flex w="100%"
-        flexDirection={["column-reverse", "column-reverse","row"]}
-        alignItems={["center","center","start"]}
-        justifyContent={["center",null]}
+export const BlogDetailContent = ({
+  title,
+  content,
+  timeToRead,
+}: IBlogDetailContent) => {
+  return (
+    <Flex
+      w="100%"
+      flexDirection={["column-reverse", "column-reverse", "row"]}
+      alignItems={["center", "center", "start"]}
+      justifyContent={["center", null]}
     >
-        <Socials></Socials>
-        <Box pt={"25px"} pl={["0px","0px","35px"]} w={["100%","80%"]}  >
-            {/* <HStack gap="10px" display={["flex"]} flexDirection={["column","column","row"]}>
-                <BlogTitle title={title}></BlogTitle>
-              <TimerCard timetoRead={timeToRead} ></TimerCard>
-            </HStack> */}
-            <Box width="100%" >
-                <Heading color="#2D2B4A"
-                    textAlign={["center","center","left"]}
-                    mt="10px" mb="10px"
-                    fontFamily="satoshi bold"
-                    fontWeight="900" fontSize={["24px", "44px"]}>
-                  {title}
-                </Heading>
-                <Text fontWeight="400" fontFamily="satoshi"
-                    color="#666481" fontSize="18px" textAlign={["center","center","left"]}>
-                    {content}
-                </Text>
-
-
-            </Box>
-           
-
+      <Socials></Socials>
+      <Box pt={"25px"} pl={["0px", "0px", "35px"]} w={["100%", "80%"]}>
+        <HStack
+          gap="10px"
+          display={["flex"]}
+          flexDirection={["column", "column", "row"]}
+        >
+          <BlogTitle title={title}></BlogTitle>
+          <TimerCard timetoRead={timeToRead}></TimerCard>
+        </HStack>
+        <Box width="100%">
+          <Heading
+            color="#2D2B4A"
+            textAlign={["center", "center", "left"]}
+            mt="10px"
+            mb="10px"
+            fontFamily="satoshi bold"
+            fontWeight="900"
+            fontSize={["24px", "44px"]}
+          >
+            {title}
+          </Heading>
+          <Box
+            fontWeight="400"
+            fontFamily="satoshi"
+            className="blog"
+            color="#666481"
+            fontSize="18px"
+            textAlign={["center", "center", "left"]}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
         </Box>
-    </Flex>)
-}
+      </Box>
+    </Flex>
+  );
+};
--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -1,157 +1,134 @@
-import { api } from "@/axios"
-import { BlogDetailContent } from "@/component/blogDetails/BlogDetailContent"
-import { BlogDetailHeader } from "@/component/blogDetails/BlogDetailHeader"
-import { SuggestedArticle } from "@/component/blogDetails/suggestedArticle"
-import { ParsedUrlQuery } from "querystring"
+import { api } from "@/axios";
+import { BlogDetailContent } from "@/component/blogDetails/BlogDetailContent";
+import { BlogDetailHeader } from "@/component/blogDetails/BlogDetailHeader";
+import { SuggestedArticle } from "@/component/blogDetails/suggestedArticle";
+// import { ParsedUrlQuery } from "querystring"
+import { parseContent } from "@/util/parser";
 
-import { Layout } from "@/layout"
-import { Box } from "@chakra-ui/react"
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { Layout } from "@/layout";
+import { Box } from "@chakra-ui/react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 //utils
-import { estimateArticleReadTime } from "@/util/estimateReadTime"
+import { estimateArticleReadTime } from "@/util/estimateReadTime";
 //hooks
-import { useRenderArticles } from "@/hooks/useRenderArticles"
-import Seo from "@/component/seo"
-import { NextPage,GetStaticProps, GetStaticPaths } from "next"
+import { useRenderArticles } from "@/hooks/useRenderArticles";
+import Seo from "@/component/seo";
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 
+type attribute = {
+  author: string;
+  authorImage: any;
+  category: any;
+  content: any;
+  createdAt: string;
+  description: string;
+  image: any;
+  locale: string;
+  localisation: any;
+  publishedAt: string;
+  slug: string;
+  title: string;
+  updatedAt: string;
+};
 
+type BlogDetailPage = {
+  article: { attributes: attribute };
+  otherArticle: attribute[];
+};
 
+type GetStaticPathsContext = {
+  locales?: string[];
+  defaultLocale?: string;
+};
 
- type attribute = {
-author: string,
-authorImage:any,
-category:any,
-content:any,
-createdAt:string,
-description:string,
-image:any,
-locale:string,
-localisation:any,
-publishedAt:string,
-slug:string,
-title:string,
-updatedAt:string
-    
-}
-
- type BlogDetailPage = {
-     article: {attributes:attribute},
-     otherArticle: attribute[]
-
- }
-
-  type GetStaticPathsContext = {
-    locales?: string[];
-    defaultLocale?: string;
+const BlogDetails: NextPage<BlogDetailPage> = ({
+  article,
+  otherArticle,
+}: BlogDetailPage) => {
+  const baseUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL as string;
+  const articleContent = article?.attributes?.content;
+  const articleHeader = article?.attributes?.title;
+  const timeToRead = estimateArticleReadTime(articleContent);
+  const imagePath = article?.attributes?.image?.data?.attributes?.url;
+  const otherArticleToRead = useRenderArticles(otherArticle);
+  const articleDescription = article?.attributes?.description;
+  const seo = {
+    metaTitle: articleHeader,
+    metaDescription: articleDescription,
+    shareImage: imagePath,
+    article: true,
   };
 
+  return (
+    <Layout>
+      <Seo {...seo} />
+      <Box w="100%" pl="5%" pr="5%">
+        <BlogDetailHeader
+          baseUrl={baseUrl}
+          imagePath={imagePath}
+        ></BlogDetailHeader>
+        <BlogDetailContent
+          title={articleHeader}
+          content={articleContent}
+          timeToRead={timeToRead}
+        ></BlogDetailContent>
+        <SuggestedArticle otherArticles={otherArticleToRead}></SuggestedArticle>
+      </Box>
+    </Layout>
+  );
+};
 
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  const { locales } = context;
 
-const BlogDetails:NextPage<BlogDetailPage> = ({article,otherArticle}:BlogDetailPage) => {
-  
-  
-    const baseUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL as string
-    const articleContent = article?.attributes?.content
-    const articleHeader = article?.attributes?.title
-    const timeToRead = estimateArticleReadTime(articleContent) 
-    const imagePath =  article?.attributes?.image?.data?.attributes?.url
-    const otherArticleToRead = useRenderArticles(otherArticle)
-    const articleDescription =  article?.attributes?.description
-    const seo = {
-        metaTitle: articleHeader,
-        metaDescription: articleDescription,
-        shareImage: imagePath,
-        article: true,
-      };
-    
+  const allArticles = await api.get(`/api/articles/?populate=*`);
+  const path: any[] = [];
 
-    return (
-        <Layout >
-            <Seo  {...seo}/>
-            <Box w="100%" pl="5%" pr="5%"  >
-                <BlogDetailHeader
-                    baseUrl={baseUrl}
-                    imagePath={imagePath}
-                    
-                ></BlogDetailHeader>
-                <BlogDetailContent
-                    title={articleHeader}
-                    content={articleContent}
-                    timeToRead={timeToRead}
-                   
-                ></BlogDetailContent>
-                <SuggestedArticle
-                otherArticles={otherArticleToRead}
-                ></SuggestedArticle>
-                
-            </Box>
-            
-        
-            
-       </Layout>
-    )
- }
-
-
-
-
-export const getStaticPaths:GetStaticPaths = async(context) => {
-   const { locales } =context;
-
-    const allArticles = await api.get(`/api/articles/?populate=*`)
-    const path: any[] = []
-    
-
-  
-    allArticles?.data?.data?.map((eachArticle: any) => {
-
-        locales!.map((language:any) => {
-            path.push({
-                params: {
-                    slug: eachArticle?.attributes?.slug
-                },
-                locale:language
-
-            })
-        })
-
-
-       
-    })
-
-
-   console.log("path",context)
- 
-    
-    return {
-        paths:path,
-        fallback:false
-}
-
-}
- 
-
-export const getStaticProps:GetStaticProps = async ({ locale, params }) => {
-       
-    const [singleArticle, otherArticle] = await Promise.all([
-            api.get(`/api/articles?filters[slug][$eq]=${params?.slug}&populate=*&locale=${locale}`),
-            api.get(`/api/articles?filters[slug][$ne]=${params?.slug}&populate=*&locale=${locale}`)
-        ])
-
-
-
-
-    return {
-        props: {
-            article: singleArticle?.data.data[0],
-            otherArticle: otherArticle?.data.data,
-            locale,
-            ...await serverSideTranslations(locale!, ['common']),
+  allArticles?.data?.data?.map((eachArticle: any) => {
+    locales!.map((language: any) => {
+      path.push({
+        params: {
+          slug: eachArticle?.attributes?.slug,
         },
-        revalidate:1
-    }
-    
-}
+        locale: language,
+      });
+    });
+  });
 
+  console.log("path", context);
 
-export default BlogDetails
+  return {
+    paths: path,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
+  const [singleArticle, otherArticle] = await Promise.all([
+    api.get(
+      `/api/articles?filters[slug][$eq]=${params?.slug}&populate=*&locale=${locale}`
+    ),
+    api.get(
+      `/api/articles?filters[slug][$ne]=${params?.slug}&populate=*&locale=${locale}`
+    ),
+  ]);
+
+  const data = singleArticle?.data.data[0];
+  if (singleArticle?.data.data[0]?.attributes?.content) {
+    singleArticle.data.data[0].attributes.content = await parseContent(
+      singleArticle?.data.data[0]?.attributes?.content
+    );
+  }
+
+  return {
+    props: {
+      article: data,
+      otherArticle: otherArticle?.data.data,
+      locale,
+      ...(await serverSideTranslations(locale!, ["common"])),
+    },
+    revalidate: 1,
+  };
+};
+
+export default BlogDetails;
