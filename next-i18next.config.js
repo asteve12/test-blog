ddc28e23fd02167139a6fc835b2e2fd7f2c78326
asteve <asteve@localhost.localdomain>
2023-02-08 02:18:29 +0100

    update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,12 @@
 
+
+const path = require('path')
 module.exports = {
     i18n: {
      
         locales: ['en','fr-BJ'],
         defaultLocale:'en',
     },
+    outputFileTracing: true,
+    localePath: path.resolve('./public/locales')
   };
