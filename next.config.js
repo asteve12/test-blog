f701b8bf3027a1e799f65393ccf15013a97c3f73
asteve <asteve@localhost.localdomain>
2023-02-08 01:37:25 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+
+const path = require('path')
 const { i18n } = require("./next-i18next.config")
 
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  outputFileTracing: true,
+  localePath: path.resolve('./public/locales')
+
 }
 module.exports = nextConfig
