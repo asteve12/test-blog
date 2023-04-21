a17ea4799abfabaf33a5b01a7d6a71e86d229fe0
asteve12 <deleade12@gmail.com>
2023-04-21 12:18:42 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import localFont from "@next/font/local"
+import { NextSeo } from 'next-seo';
+
 
 
 //@ts-ignore
@@ -54,7 +56,9 @@ function App({ Component, pageProps }: AppProps) {
   const { initialise } = usePreventHydrationError();
 
   if (!initialise) return  <Head>
-    <Seo {...seo} />
+ <Seo  {...seo}/>
+
+    
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charSet="utf-8" />
 <link
