d2af9836547f2fcdd682a995bb48a343c13d9454
asteve12 <deleade12@gmail.com>
2023-04-21 09:53:19 +0100

    update blog

--- src/component/seo.tsx
+++ src/component/seo.tsx
@@ -9,34 +9,25 @@ type ISeo = {
 };
 
 const Seo = (SEO: ISeo) => {
+  
 
 
   return (
     <>
 
-      {SEO.metaTitle && (
-        <>
-          <title>{SEO.metaTitle}</title>
+<title>{SEO.metaTitle}</title>
           <meta property="og:title" content={SEO.metaTitle} />
           <meta name="twitter:title" content={SEO.metaTitle} />
-        </>
-      )}
-      {SEO.metaDescription && (
-        <>
           <meta name="description" content={SEO.metaDescription} />
           <meta property="og:description" content={SEO.metaDescription} />
           <meta name="twitter:description" content={SEO.metaDescription} />
-        </>
-      )}
-      {SEO.shareImage && (
-        <>
           <meta property="og:image" content={SEO.shareImage} />
           <meta name="twitter:image" content={SEO.shareImage} />
           <meta name="image" content={SEO.shareImage} />
-        </>
-      )}
-      {SEO.article && <meta property="og:type" content="article" />}
-      <meta name="twitter:card" content={SEO.shareImage} />
+          <meta property="og:type" content="article" />
+         <meta name="twitter:card" content={SEO.shareImage} />
+  
+      
     </>
   );
 };
